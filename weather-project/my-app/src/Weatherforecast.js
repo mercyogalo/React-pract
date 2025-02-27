c8121ea83a76ca427d82b020c97a9ae2b6224401
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2025-02-27 12:07:17 +0300

    updated weather  forecast
    updated the function to display the forecast

--- weather-project/my-app/src/Weatherforecast.js
+++ weather-project/my-app/src/Weatherforecast.js
@@ -1,66 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "./App.css"
+import axios from "axios"
 
 export default function Weatherforecast(props){
+    let [loaded,setLoaded]=useState(false);
+    let [forecast,setForecast]=useState(null);
 
+function handleResponse(response){
+    setForecast(response.data.daily);
+    setLoaded(true);
+}
+
+if (loaded && forecast && forecast.length > 0) {
+    console.log({forecast});
     return (
         <div className="Weatherforecast container">  
          <div className="row  forecast-section">  
 
-<div className="col-md-1 eachSection ">
+
+    <div className="col-md-1 eachSection ">
     <p>Thursday</p>
-    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png" alt="weather-icon" />
+    <img src={`${forecast[0].condition.icon_url}`} alt="weather-icon" />
     <br/>
-    <span className="me-4">18</span><span>20</span>
+    <span className="me-4">{forecast[0].temperature.minimum}</span><span>{forecast[0].temperature.maximum}</span>
 </div>
 
-<div className="col-md-1 eachSection">
-    <p>Thursday</p>
-    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png" alt="weather-icon" />
-    <br/>
-    <span className="me-4">18</span><span>20</span>
-</div>
-
-
-<div className="col-md-1 eachSection">
-    <p>Thursday</p>
-    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png" alt="weather-icon" />
-    <br/>
-    <span className="me-4">18</span><span>20</span>
-</div>
-
-
-<div className="col-md-1 eachSection">
-    <p>Thursday</p>
-    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png" alt="weather-icon" />
-    <br/>
-    <span className="me-4">18</span><span>20</span>
-</div>
-
-<div className="col-md-1 eachSection">
-    <p>Thursday</p>
-    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png" alt="weather-icon" />
-    <br/>
-    <span className="me-4">18</span><span>20</span>
-</div>
-
-
-
-<div className="col-md-1 eachSection">
-    <p>Thursday</p>
-    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png" alt="weather-icon" />
-    <br/>
-    <span className="me-4">18</span><span>20</span>
-</div>
-
-
-
-<div className="col-md-1 eachSection">
-    <p>Thursday</p>
-    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png" alt="weather-icon" />
-    <br/>
-    <span className="me-4">18</span><span>20</span>
-</div>
 
 
 
@@ -70,4 +34,21 @@ export default function Weatherforecast(props){
 
 
     );
+
+
+}
+else{
+
+    let apiKey=`tba3e25ffd38f7e512a5944489457o05`
+    let apiUrl=`https://api.shecodes.io/weather/v1/forecast?lat=${props.latitude}&lon=${props.longitude}&key=${apiKey}`
+    axios.get(apiUrl).then(handleResponse);
+
+
+
+}
+
+
+
+
+  
 }
